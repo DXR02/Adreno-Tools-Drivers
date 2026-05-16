fe940fc779d3317b9e1f04d9e9206188a52bf096
DXR02 <96510562+DXR02@users.noreply.github.com>
2026-05-16 10:22:07 -0600

    Change git checkout branch to 'turnip/gen8'

--- build_turnip.sh
+++ build_turnip.sh
@@ -41,7 +41,7 @@ prepare_workdir(){
 
 build_lib_for_android(){
     cd "$workdir/$srcfolder"
-    git checkout -f "origin/$1"
+    git checkout -f "origin/turnip/gen8"
 
     sed -i 's/ (%s)//g' src/freedreno/vulkan/tu_device.cc || true
     sed -i 's/ (%s)//g' src/freedreno/vulkan/tu_device.c || true
